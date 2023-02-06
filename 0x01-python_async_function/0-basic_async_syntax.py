76a41f79265f03a3f11406bf55ba6abb36563e84
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-02-06 21:40:28 +0100

    go

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     ''' wait for a random number of  secomds
     '''
 
